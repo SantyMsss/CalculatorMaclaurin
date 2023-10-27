163e2aed477fde8f7da343d88d365e96fe7512e0
Santiago Martinez <santiagomarse@hotmail.com>
2023-10-26 21:43:15 -0500

    se arregla menu

--- PruebaMaclaurin.java
+++ PruebaMaclaurin.java
@@ -1,37 +1,52 @@
 import javax.swing.JOptionPane;
 
+/* 
+ *  CALCULADORA IMPLEMENTADA PARA LA ASIGNATURA DE MATEMATICAS III
+ * SE CUMPLEN CON LAS FUNCIONES DEL SENO, COSENO, LN Y EULER CUANDO N=10
+ * EL VALOR DE X ES ASIGNADO POR EL USUARIO
+ * 
+ *
+ * DESARROLLADO POR:
+ * UNIDAD CENTRAL DEL VALLE DEL CAUCA
+ * SANTIAGO MARTINEZ SERNA
+ * LAURA SOFIA TORO GARCIA
+ * SANTIAGO ALEJANDRO SANTACRUZ
+ * 
+*/
+
 
 public class PruebaMaclaurin {
 
 
-
-    public void calculateAndDisplayResult() {
-
+    public void Calculadora() {
 
 
+        // Seleccionar la función a aproximar   
+        do{
         int funcion = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la función a aproximar:\n"
                 + "1. seno\n"
                 + "2. coseno\n"
-                + "3. \n"
-                + "4. \n"));
+                + "3. Ln\n"
+                + "4. Euler\n"));
                 
-
+        // Ingresar el valor de x
         double x = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor de x:"));
         double resultado = 0.0;
 
+        // Calcular la aproximación
         switch (funcion) {
             case 1:
-                resultado = calcularAproximacionSeno(x);
+                resultado = Seno(x);
                 break;
             case 2:
-                resultado = calcularAproximacionCoseno(x);
-                
+                resultado = Coseno(x);
+
                 break;
             case 3:
-                resultado = (x);
+                resultado = Ln(x);
                 break;
             case 4:
-                resultado = (x);
+                resultado = Exponencial(x);
                 break;
             default:
                 JOptionPane.showMessageDialog(null, "Error: Opción inválida.");
@@ -40,9 +55,14 @@ public class PruebaMaclaurin {
 
         
         JOptionPane.showMessageDialog(null, "El resultado es de la aproximacion es: " + resultado);
+    }while(JOptionPane.showConfirmDialog(null, "¿Desea realizar otra operación?")==0);
     }
 
-    public double calcularAproximacionExponencial(double x) {
+    // INICIO DE FUNCIONES MATEMÁTICAS
+
+
+    // FUNCION EULER
+    public double Exponencial(double x) {
         double aproximacion = 0.0;
         for (int n = 0; n < 10; n++) {
             aproximacion += Math.pow(x, n) / factorial(n);
@@ -50,7 +70,8 @@ public class PruebaMaclaurin {
         return aproximacion;
     }
 
-    public double calcularAproximacionLogaritmoNatural(double x) {
+    // FUNCION LOGARITMO NATURAL
+    public double Ln(double x) {
         if (x <= 0) {
             JOptionPane.showMessageDialog(null, "Error: El logaritmo natural no está definido para números no positivos.");
             System.exit(1);
@@ -67,7 +88,9 @@ public class PruebaMaclaurin {
         return aproximacion;
     }
 
-    public double calcularAproximacionSeno(double x) {
+
+    // FUNCION SENO
+    public double Seno(double x) {
         double aproximacion = 0.0;
         for (int n = 0; n < 10; n++) {
             aproximacion += Math.pow(-1, n) * Math.pow(x, 2 * n + 1) / factorial(2 * n + 1);
@@ -75,14 +98,18 @@ public class PruebaMaclaurin {
         return aproximacion;
     }
 
-    public double calcularAproximacionCoseno(double x) {
+    // FUNCION COSENO
+    // OJO NO SE ESTA DANDO EL RESULTADO CORRECTO
+    public double Coseno(double x) {
         double aproximacion = 0.0;
         for (int n = 0; n < 10; n++) {
             aproximacion += Math.pow(-1, n) * Math.pow(x, 2 * n) / factorial(2 * n);
         }
         return aproximacion;
-    }
+}
 
+
+    // FUNCION FACTORIAL
     public double factorial(int n) {
         if (n == 0) {
             return 1;
@@ -97,8 +124,8 @@ public class PruebaMaclaurin {
 
 
      public static void main(String[] args) {
-         PruebaMaclaurin calculator = new PruebaMaclaurin();
-        calculator.calculateAndDisplayResult();
+         PruebaMaclaurin cl = new PruebaMaclaurin();
+        cl.Calculadora();
     }
 
 }
